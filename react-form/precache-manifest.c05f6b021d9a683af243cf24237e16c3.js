17acc202af13177b0a0242512130f73cbb898482
domind <domind1@gmail.com>
2019-06-30 21:29:07 +0300

    updated React projects

---
+++ react-form/precache-manifest.c05f6b021d9a683af243cf24237e16c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea39f64a282db1c34a251f65207e7c78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af30aa1de541f677ce6c",
+    "url": "/static/css/main.c31ee7ad.chunk.css"
+  },
+  {
+    "revision": "9f167f835e7c16cf8fb3",
+    "url": "/static/js/2.a09c7072.chunk.js"
+  },
+  {
+    "revision": "af30aa1de541f677ce6c",
+    "url": "/static/js/main.07f946ad.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

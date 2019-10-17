4d1f385e8ba7e76b093fe1dec9cd29780d6d9adc
domind <domind1@gmail.com>
2019-10-17 21:59:09 +0300

    Added KML editor

---
+++ maps/precache-manifest.66418861a1641e25f541f640e0e576bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8e1941623b7be7cac0f7d4b8963b01f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01e807a05ab16bd643d8",
+    "url": "/static/css/2.d0b16a99.chunk.css"
+  },
+  {
+    "revision": "176f93ba289121a6c0a2",
+    "url": "/static/css/main.6bea51b0.chunk.css"
+  },
+  {
+    "revision": "01e807a05ab16bd643d8",
+    "url": "/static/js/2.7954c2f9.chunk.js"
+  },
+  {
+    "revision": "176f93ba289121a6c0a2",
+    "url": "/static/js/main.e4fce74c.chunk.js"
+  },
+  {
+    "revision": "86afbd74552bca5f6a6d",
+    "url": "/static/js/runtime-main.2d661999.js"
+  }
+]);

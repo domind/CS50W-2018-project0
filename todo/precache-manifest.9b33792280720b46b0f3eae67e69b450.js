cf0069346101d09d959291856b74227c2f172ec3
domind <domind1@gmail.com>
2019-11-25 20:53:00 +0200

    correction of app path'

---
+++ todo/precache-manifest.9b33792280720b46b0f3eae67e69b450.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59775dd4644370d016eff304e0a7a364",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "fbecc866cb91364421f1",
+    "url": "/todo/static/css/main.57858b57.chunk.css"
+  },
+  {
+    "revision": "d898f2404c533a7a93f3",
+    "url": "/todo/static/js/2.1613fa9a.chunk.js"
+  },
+  {
+    "revision": "fbecc866cb91364421f1",
+    "url": "/todo/static/js/main.5a176f55.chunk.js"
+  },
+  {
+    "revision": "5284980906ec5d953cee",
+    "url": "/todo/static/js/runtime-main.8be1e5ce.js"
+  }
+]);
--- todo/service-worker.js
+++ todo/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2388c1f1b5c9e32c51843392b109067e.js"
+  "/todo/precache-manifest.9b33792280720b46b0f3eae67e69b450.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

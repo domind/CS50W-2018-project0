3df51ce5e762bdf880757b60b3265f5828d46b4a
domind <domind1@gmail.com>
2020-06-14 21:18:05 +0300

    currency-converter

---
+++ currency-converter/precache-manifest.5cd9654dcc2adcb43ad436c9c32b4348.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42bd4283f8e553a838dd656b3eeec0cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "846513fe18f6f8e28d31",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a6ea1c45c893b4c637e7",
+    "url": "/static/css/main.488cdf52.chunk.css"
+  },
+  {
+    "revision": "846513fe18f6f8e28d31",
+    "url": "/static/js/2.eefc10f9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.eefc10f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6ea1c45c893b4c637e7",
+    "url": "/static/js/main.dd1fffcc.chunk.js"
+  },
+  {
+    "revision": "d378da2467e05fd4b3f8",
+    "url": "/static/js/runtime-main.f31af614.js"
+  }
+]);
